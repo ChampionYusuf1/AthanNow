8e2786dbe84dac4cc7989d43258fd3e8743cbb5f
Yusuf Ghani <championyusuf1@gmail.com>
2024-05-21 09:53:13 -0500

    Now, User location is dynamic so if a user in the past already stored their location, location services are not needed, function is already written if user wants to change location to remove their old latitude and longitude

--- athannow/athannow/lib/commonfunctions/functins.dart
+++ athannow/athannow/lib/commonfunctions/functins.dart
@@ -1,62 +1,88 @@
 import 'package:flutter/cupertino.dart';
 import 'package:location/location.dart' as loc;
 import 'package:geolocator/geolocator.dart' as geo;
+import 'package:shared_preferences/shared_preferences.dart';
 
-void requestLocationPermissionAndLogCoordinates(BuildContext context) {
-  showCupertinoDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return CupertinoAlertDialog(
-        title: Text('Location Permission'),
-        content: Text('My App wants to access your location.'),
-        actions: [
-          CupertinoDialogAction(
-            child: Text('Deny'),
-            onPressed: () {
-              print('Deny pressed');
-              Navigator.of(context).pop();
-            },
-          ),
-          CupertinoDialogAction(
-            child: Text('Allow'),
-            onPressed: () async {
-              print('Allow pressed');
-              Navigator.of(context).pop();
-              await getLocationPermission(context);
-              await getUserLocation(context);
-            },
-          ),
-        ],
-      );
-    },
-  );
+Future<void> requestLocationPermissionAndLogCoordinates(
+    BuildContext context) async {
+  bool location = await isDataAlreadyHere();
+  print("Location: $location");
+
+  if (!location) {
+    showCupertinoDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return CupertinoAlertDialog(
+          title: const Text('Location Permission'),
+          content: const Text('My App wants to access your location.'),
+          actions: [
+            CupertinoDialogAction(
+              child: Text('Deny'),
+              onPressed: () {
+                print('Deny pressed');
+                Navigator.of(context, rootNavigator: true).pop();
+              },
+            ),
+            CupertinoDialogAction(
+              child: Text('Allow'),
+              onPressed: () async {
+                print('Allow pressed');
+                Navigator.of(context, rootNavigator: true).pop();
+                await handleLocationPermission(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  } else {
+    print("Location already stored");
+    //   removeLocationData();
+  }
 }
 
-Future<void> getLocationPermission(BuildContext context) async {
+Future<void> handleLocationPermission(BuildContext context) async {
+  print('Handling location permission');
+  try {
+    await getLocationPermission();
+    print('Location permission handled');
+    await getUserLocation();
+    print('Location retrieved successfully');
+  } catch (e) {
+    print('Error during permission handling or location retrieval: $e');
+  }
+}
+
+Future<void> getLocationPermission() async {
+  print('Requesting location permission');
   loc.Location location = loc.Location();
   bool serviceEnabled;
   loc.PermissionStatus permissionStatus;
 
   serviceEnabled = await location.serviceEnabled();
   if (!serviceEnabled) {
+    print('Location service not enabled, requesting enablement');
     serviceEnabled = await location.requestService();
     if (!serviceEnabled) {
-      print('Location services not enabled');
+      print('Location service not enabled by user');
       return;
     }
   }
 
   permissionStatus = await location.hasPermission();
   if (permissionStatus == loc.PermissionStatus.denied) {
+    print('Location permission denied, requesting permission');
     permissionStatus = await location.requestPermission();
     if (permissionStatus != loc.PermissionStatus.granted) {
-      print('Location permission denied');
+      print('Location permission denied by user');
       return;
     }
   }
+  print('Location permission granted');
 }
 
-Future<void> getUserLocation(BuildContext context) async {
+Future<void> getUserLocation() async {
+  print('Requesting user location');
   try {
     geo.Position position = await geo.Geolocator.getCurrentPosition(
       desiredAccuracy: geo.LocationAccuracy.high,
@@ -68,7 +94,38 @@ Future<void> getUserLocation(BuildContext context) async {
     // Log the latitude and longitude values
     print('Latitude: $latitude');
     print('Longitude: $longitude');
+    await storeLocationData(latitude, longitude);
   } catch (e) {
     print('Error getting location: $e');
   }
+}
+
+Future<void> storeLocationData(double latitude, double longitude) async {
+  final userPreference = await SharedPreferences.getInstance();
+
+  await userPreference.setDouble('latitude', latitude);
+  await userPreference.setDouble('longitude', longitude);
+  print("Memory stored correctly");
+}
+
+Future<bool> isDataAlreadyHere() async {
+  final userPreference = await SharedPreferences.getInstance();
+
+  final double? latitude2 = userPreference.getDouble('latitude');
+  final double? longitude2 = userPreference.getDouble('longitude');
+  print('Latitude memory check: $latitude2');
+  print('Longitude memory check: $longitude2');
+
+  if (latitude2 == null || longitude2 == null) {
+    return false;
+  }
+  return true;
+}
+
+// removing location data
+Future<void> removeLocationData() async {
+  final userPreference = await SharedPreferences.getInstance();
+  await userPreference.remove('latitude');
+  await userPreference.remove('longitude');
+  print("Memory removed correctly");
 }
