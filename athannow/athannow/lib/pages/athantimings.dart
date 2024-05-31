9bab2fd1b7e77e2d8dade5dbc7b3c6b9656af847
Yusuf Ghani <championyusuf1@gmail.com>
2024-05-31 12:09:43 -0500

    table UI changes

--- athannow/athannow/lib/pages/athantimings.dart
+++ athannow/athannow/lib/pages/athantimings.dart
@@ -19,33 +19,47 @@ class _AthanTimingsPageState extends State<AthanTimingsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Athan Timings'),
+        title: Text('AthanNow'),
       ),
-      body: Center(
+      body: Padding(
+        //  padding: const EdgeInsets.all(10.0),
+        padding: const EdgeInsetsDirectional.fromSTEB(20, 1, 20, 1),
         child: FutureBuilder<NamazTimings>(
           future: futureNamazTimings,
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
+              return Center(child: CircularProgressIndicator());
             } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
+              return Center(child: Text('Error: ${snapshot.error}'));
             } else if (!snapshot.hasData) {
-              return Text('No data found');
+              return Center(child: Text('No data found'));
             } else {
               final namazTimings = snapshot.data!;
-              return Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Table(
-                  border: TableBorder.all(color: Colors.grey),
-                  children: [
-                    _buildTableRow('Prayer', 'Time', isHeader: true),
-                    _buildTableRow('Fajr', namazTimings.fajr ?? ''),
-                    _buildTableRow('Dhuhr', namazTimings.dhuhr ?? ''),
-                    _buildTableRow('Asr', namazTimings.asr ?? ''),
-                    _buildTableRow('Maghrib', namazTimings.maghrib ?? ''),
-                    _buildTableRow('Isha', namazTimings.isha ?? ''),
-                  ],
-                ),
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Center(
+                    child: Text(
+                      'Prayer Timings',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Table(
+                    border: TableBorder.all(color: Colors.grey),
+                    children: [
+                      _buildTableRow('Prayer', 'Time', isHeader: true),
+                      _buildTableRow('Fajr', namazTimings.fajr!),
+                      _buildTableRow('Dhuhr', namazTimings.dhuhr!),
+                      _buildTableRow('Asr', namazTimings.asr!),
+                      _buildTableRow('Maghrib', namazTimings.maghrib!),
+                      _buildTableRow('Isha', namazTimings.isha!),
+                    ],
+                  ),
+                ],
               );
             }
           },
@@ -58,22 +72,22 @@ class _AthanTimingsPageState extends State<AthanTimingsPage> {
     return TableRow(
       children: [
         Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(16.0),
           child: Text(
             name,
             style: TextStyle(
-              fontSize: 16,
+              fontSize: 18,
               fontWeight: isHeader ? FontWeight.bold : FontWeight.normal,
               color: isHeader ? Colors.black : Colors.grey[700],
             ),
           ),
         ),
         Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(16.0),
           child: Text(
             time,
             style: TextStyle(
-              fontSize: 16,
+              fontSize: 18,
               fontWeight: isHeader ? FontWeight.bold : FontWeight.normal,
               color: isHeader ? Colors.black : Colors.grey[700],
             ),
